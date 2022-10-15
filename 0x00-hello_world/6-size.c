e36f2d86dca967943fd6af279a39a832fa8302eb
Olere John <Johnolere96@gmail.com>
2022-10-15 11:27:55 -0700

    printing size in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *main -is the entry point for the program.
+ * main - Entry point
  *
- *Return:zero if no error, Non-zero value if errors.
+ * Return: always 0
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-        printf("Size of an int: %d byte(s)\n", sizeof(int));
-        printf("Size of long int: %d byte(s)\n", sizeof(long int));
-        printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %d byte(s)\n", sizeof(float));
-        return (o);
-}	
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
