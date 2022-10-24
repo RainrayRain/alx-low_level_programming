0464a4eb769a45c933ac9775dc717ca45f3ce577
Olere John <Johnolere96@gmail.com>
2022-10-24 10:01:59 -0700

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -reset to 98
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	*n = 98
+
+	n = 402;
+	putchar("n=%d\n" , n);
+		putchar("n=%d\n" , &n);
+		return (0);
+}
