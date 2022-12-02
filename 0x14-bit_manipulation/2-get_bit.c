a34b4ec1ca750b6e6a996ceea9cbbaad8178d946
RainrayRain <johnolere96@gmail.com>
2022-12-02 17:11:05 -0600

    add bit file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit -value of a bit at a given index
+ * get_bit - value of a bit at a given index
  * @n: decimal parameter
  * @index: index
  * Return: val
@@ -10,8 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
-	bit - (n >> index);
+	bit = (n >> index);
 	if (index > 32)
-		return (-1);
+	return (-1);
 	return (bit & 1);
 }
