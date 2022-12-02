848711092577cc966cc6f79fc20ca45d846382dc
RainrayRain <johnolere96@gmail.com>
2022-12-02 17:38:01 -0600

    bit file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * set_bit - set the value of a bit to 1 at a given index
+ * set_bit - sets the value of a bit to 1 at a given index
  * @n: parameter
  * @index: index
  * Return: value
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > siezeof(n) * 8)
+	if (index > sizeof(n) * 8)
 		return (-1);
 	*n |= (1 << index);
 	return (1);
